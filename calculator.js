99423b8df2342a3d9ffb0ea3b8da3a30f2a476d3
Tech-A35 <tech-a35@Tech-A35s-MacBook-Air.local>
2018-04-23 16:55:18 +0100

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,9 @@
+var which-calculator = prompt("Which calculator would you like to use? Type B for basic or A for advanced.");
+if (which-calculator == "B"){
+  // get the user inputs
+  var value1 = prompt('enter first value');
+  var value2 = prompt('enter the second value');
+  var choice = prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide (%)modulus') || "a";
+
+
+}
